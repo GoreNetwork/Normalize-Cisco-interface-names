b73de51fe761d012d40b5b2ed5bc88269ce5fc64
GoreNetwork <dhimes@gmail.com>
2017-08-31 14:13:47 -0400

    Update name_work.py

--- name_work.py
+++ name_work.py
@@ -1,25 +1,3 @@
-import re
-
-
-def modified_for_chassis_read_doc (file_name):
-	doc = []
-	for line in open(file_name, 'r').readlines():
-		if "PID" in line:
-			line = "  "+ line
-		doc.append(line)
-	return doc
-
-def strip_duke_energy_com(line):
-	start_of_duke = re.search("[.][dD][uU][kK][eE]?[-]?[eE]?[nN]?[eE]?[rR]?[gG]?[yY]?[.]?[cC]?[oO]?[mM]?", line)
-	
-	if start_of_duke == None:
-		return (line)
-	else:
-		temp_name = line[:start_of_duke.start()]
-		return temp_name
-
-#def find_SFP_type(interface,show_int_stat_file):
-
 
 def split_interface(interface):
 	num_index = interface.index(next(x for x in interface if x.isdigit()))
@@ -39,15 +17,6 @@ def normalize_interface_names(non_norm_int):
 					return return_this
 	return "normalize_interface_names Failed"
 			
-sfps = [
-
-	[["1000BaseSX"],"GLC-SX-MMD"],
-	[["1000BaseLX"],"GLC-LH-SMD"],
-	[["10/100BaseTX","10/100/1000-TX"],"GLC-T"],
-	[[1],"GLC-GE-100FX"],
-	[[1],"SFP-10G-SR"],
-	[[1],"SFP-10G-LR"],
-]
 
 interfaces = [
 	[["Ethernet","Eth"],"Eth"],
